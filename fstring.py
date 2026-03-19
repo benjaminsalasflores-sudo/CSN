1da09e92a771c2629a0b83c56f7523e971984b3d
benjaminsalasflores-sudo <benjamin.salas.flores@cpsn.cl>
2026-03-19 10:44:02 -0300

    hola commit

--- fstring.py
+++ fstring.py
@@ -2,27 +2,27 @@
 
 # Declaración
 
-frase = "Anita lava la tina"
+# frase = "Anita lava la tina"
 nombre = "Carlos"
 
 
 # Concatenación
 print("Hola, " + nombre)          # con +
-print("Hola,", nombre)            # con coma
-print(f"Hola, {nombre}!")         # f-string ✅ recomendado
-print("Hola, {}!".format(nombre)) # .format()
+# print("Hola,", nombre)            # con coma
+# print(f"Hola, {nombre}!")         # f-string ✅ recomendado
+# print("Hola, {}!".format(nombre)) # .format()
 
 # Longitud y acceso por índice
-print(len(frase))    # 18
-print(frase[0])      # 'A'  (primer carácter)
-print(frase[-1])     # 'a'  (último carácter)
-print(frase[0:5])    # 'Anita' (slicing)
+# print(len(frase))    # 18
+# print(frase[0])      # 'A'  (primer carácter)
+# print(frase[-1])     # 'a'  (último carácter)
+# print(frase[0:5])    # 'Anita' (slicing)
 
 # Métodos útiles
-frase.upper()        # ANITA LAVA LA TINA
-frase.lower()        # anita lava la tina
-frase.replace("Anita","Bob")    # Bob lava la tina
-frase.split(" ")     # ['Anita', 'lava', 'la', 'tina']
-"  hola  ".strip()   # 'hola'  (sin espacios)
-frase.startswith("A")  # True
-"Python" in frase      # False
+# frase.upper()        # ANITA LAVA LA TINA
+# frase.lower()        # anita lava la tina
+# frase.replace("Anita","Bob")    # Bob lava la tina
+# frase.split(" ")     # ['Anita', 'lava', 'la', 'tina']
+# "  hola  ".strip()   # 'hola'  (sin espacios)
+# frase.startswith("A")  # True
+# "Python" in frase      # False
